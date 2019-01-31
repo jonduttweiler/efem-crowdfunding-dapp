5c42266776336e0fc50f9a3157b868a54161dac4
Vojtech Simetka <vojtech@simetka.cz>
2019-01-30 23:55:42 -0300

    Whenever in doubt, just pour in some extra gas....

--- src/lib/blockchain/extraGas.js
+++ src/lib/blockchain/extraGas.js
@@ -1,3 +1,12 @@
 const { REACT_APP_ENVIRONMENT = 'localhost' } = process.env;
 // need to supply extraGas b/c https://github.com/trufflesuite/ganache-core/issues/26
-export default () => (REACT_APP_ENVIRONMENT !== 'localhost' ? 0 : 1000000);
+export default () => {
+  switch (REACT_APP_ENVIRONMENT) {
+    case 'localhost':
+      return 1000000;
+    case 'rsk_testnet':
+      return 100000;
+    default:
+      return 0;
+  }
+};
