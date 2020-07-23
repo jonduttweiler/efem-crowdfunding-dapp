89e2a4ad7de016b0a1254209d5811b343cde7307
jona <jonatanduttweiler@gmail.com>
2020-07-23 20:55:23 -0300

    Update RoleProvider

--- src/contextProviders/RoleProvider.js
+++ src/contextProviders/RoleProvider.js
@@ -1,12 +1,15 @@
 import React, { Component } from "react";
-import { GIVER, CREATE_DAC_ROLE, CREATE_CAMPAIGN_ROLE } from "../constants/Role";
+import { GIVER, CREATE_DAC_ROLE, CREATE_CAMPAIGN_ROLE, CREATE_MILESTONE_ROLE } from "../constants/Role";
+import PropTypes from 'prop-types';
+import User from "../models/User";
 
 const { Provider, Consumer } = React.createContext();
 export { Consumer };
 
 export default class RoleProvider extends Component {
+    //we'll get a user as prop. Vamos a agarrar ese usuario y se lo vaamos a pasar al user service para obtener los roles activos
 
-    constructor(props) { //we'll need a user as prop
+    constructor(props) {
         super(props);
         this.state = {
             roles: [GIVER]
@@ -14,7 +17,7 @@ export default class RoleProvider extends Component {
     }
     
     componentDidMount(){
-        this.setState({roles:[GIVER,CREATE_DAC_ROLE,CREATE_CAMPAIGN_ROLE]});
+        this.setState({roles:[GIVER,CREATE_CAMPAIGN_ROLE,CREATE_MILESTONE_ROLE]});
     }
 
     render(){
@@ -24,6 +27,14 @@ export default class RoleProvider extends Component {
             </Provider>
         );
     }
-
-
 }
+
+/* 
+RoleProvider.PropTypes = {
+    currentUser: PropTypes.instanceOf(User).isRequired
+}
+
+JoinGivethCommunity.defaultProps = {
+    currentUser: new User(), //?
+  };
+   */
