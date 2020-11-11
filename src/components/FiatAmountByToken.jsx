3d8fe223310fb1410a730d268e9c3936795550bd
jona <jonatanduttweiler@gmail.com>
2020-11-11 19:16:46 -0300

    Read token symbol from config

--- src/components/FiatAmountByToken.jsx
+++ src/components/FiatAmountByToken.jsx
@@ -1,23 +1,27 @@
 import React, { useState, useEffect } from 'react';
 import { Typography } from '@material-ui/core';
 import { useSelector } from 'react-redux';
+import { selectExchangeRateByToken } from '../redux/reducers/exchangeRatesSlice';
 import FiatAmount from "./FiatAmount";
 import BigNumber from 'bignumber.js';
 import web3 from 'web3';
+import config from '../configuration';
 
 const RBTCAddress = '0x0000000000000000000000000000000000000000';
 
 {/* recibe una cantidad de tokens y muestre el equivalente en fiat */ }
 const FiatAmountByToken = ({ tokenAddress = RBTCAddress, tokenAmount }) => {
     const [fiatAmount, setFiatAmount] = useState();
-    const { rate } = useSelector(state => state.exchangeRates.find(exr => exr.tokenAddress === tokenAddress))
+    const { rate } = useSelector(state => selectExchangeRateByToken(state, tokenAddress)); 
+    const {symbol} = config.tokens[tokenAddress];
+
 
     useEffect(() => {
         try {
             tokenAmount = (tokenAmount == "") ? 0 : tokenAmount;
             const asWei = web3.utils.toWei(tokenAmount.toString());
             const tokenAmountWei = new BigNumber(asWei);
-            const centsFiatAmount = tokenAmountWei.dividedBy(new BigNumber(rate));
+            const centsFiatAmount = tokenAmountWei.dividedBy(rate);
             setFiatAmount(centsFiatAmount.toString());
         } catch (err) {
             console.log(err)
@@ -26,7 +30,7 @@ const FiatAmountByToken = ({ tokenAddress = RBTCAddress, tokenAmount }) => {
 
     return (
         <Typography variant="body1">
-            {tokenAmount} [RBTC] = <FiatAmount amount={new BigNumber(fiatAmount)}/>
+            {tokenAmount} [{symbol}] = <FiatAmount amount={new BigNumber(fiatAmount)}/>
         </Typography>
     )
 }
